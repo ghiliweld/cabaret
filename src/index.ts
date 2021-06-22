9fe19f63b552c9b283a730b6101851b204e2a5d5
ganer <ganeralkidz@gmail.com>
2021-06-22 19:19:30 -0400

    commented greg's code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,61 @@
-import { match } from 'ts-pattern';
+import { match } from "ts-pattern";
 
 export interface Shelf<T> {
-    value: T | { [key: string] : Shelf<T> },
-    version: number
+  value: T | { [key: string]: Shelf<T> };
+  version: number;
 }
+
+/* 
+    shelf.merge = (a, b, dont_modify) => {
+        let change = null
+
+        if (!a) a = [null, -1]
+
+        if (!Array.isArray(b)) b = [b]
+
+        let both_objs = is_obj(a[0]) && is_obj(b[0])
+
+        !If the version of b is null
+        if (b[1] == null) b = [b[0], a[1] + (both_objs ? 0 : 1)]
+
+        !else if the version of b is 'add'
+        else if (b[1] == 'add') b = [a[0] + b[0], a[1] + 1]
+
+        !If the version of b is larger than the version of a, or if a and b have the same version but b has a greater value
+        if (b[1] > (a[1] ?? -1) || (b[1] == a[1] && greater_than(b[0], a[0]))) {
+            if (is_obj(b[0])) {
+                if (!dont_modify) {
+                    a[0] = {}
+                    a[1] = b[1]
+                }
+                change = shelf.merge(dont_modify ? [{}, b[1]] : a, b, dont_modify)
+                if (!change) change = [{}, b[1]]
+            } else {
+                if (!dont_modify) {
+                    a[0] = b[0]
+                    a[1] = b[1]
+                }
+                change = b
+            }
+        !Else if a and b have the same version and theyre both objects
+        } else if (b[1] == a[1] && both_objs) {
+            !Loop through key shelf pairs of b
+            for (let [k, v] of Object.entries(b[0])) {
+                !If we can modify and a has no shelf at the current key
+                if (!dont_modify && !a[0][k]) a[0][k] = [null, -1]
+                !Assign diff to be the result of merging a[0][k] and the current shelf for b
+                let diff = shelf.merge(a[0][k], v, dont_modify)
+                if (diff) {
+                    !If diff is not null
+                    if (!change) change = [{}, b[1]]
+                    change[0][k] = diff
+                }
+            }
+        }
+            return change
+    }
+*/
+
 
 /*
     match (a, b) with
@@ -33,6 +85,8 @@ export interface Shelf<T> {
     note: "max" isn't a mathematical max function, we just mean keep the "greatest" object,
     wtv we define that to be
 */
+
+
 export const merge = <T>(a: Shelf<T>, b: Shelf<T>): Shelf<T> => {
-    return a;
-}
+  return a;
+};
